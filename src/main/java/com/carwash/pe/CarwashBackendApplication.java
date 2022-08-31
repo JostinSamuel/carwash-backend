955e1a5cb20020de50da8cc6a8e87c4c15f0aa34
Jostin Samuel <jostinmartinez1605@gmail.com>
2022-08-31 16:55:57 -0500

    first commit

---
+++ src/main/java/com/carwash/pe/CarwashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carwash.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carwash/pe/CarwashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carwash.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
