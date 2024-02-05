012fab739e245ca9eb824fabaad0ab30bd5da747
Jostin Samuel <jostinmartinez1605@gmail.com>
2024-02-05 01:06:44 -0500

    add cross origin

--- src/main/java/com/carwash/pe/controller/ServicioController.java
+++ src/main/java/com/carwash/pe/controller/ServicioController.java
@@ -2,14 +2,7 @@ package com.carwash.pe.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.carwash.pe.model.Servicio;
 import com.carwash.pe.service.ServicioService;
@@ -17,6 +10,7 @@ import com.carwash.pe.service.ServicioService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/servicios")
 public class ServicioController {
