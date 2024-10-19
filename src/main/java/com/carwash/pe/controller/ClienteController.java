0c3349050b3ba57cbbf1b260b81631084dc72101
Marcos Basurto <marcosbr301@gmail.com>
2024-10-19 14:34:59 -0500

    No cors ClienteController.java

--- src/main/java/com/carwash/pe/controller/ClienteController.java
+++ src/main/java/com/carwash/pe/controller/ClienteController.java
@@ -3,14 +3,7 @@ package com.carwash.pe.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.carwash.pe.model.Cliente;
 import com.carwash.pe.service.ClienteService;
@@ -21,6 +14,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/clientes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200") // Permite solicitudes desde el puerto 4200
 public class ClienteController {
     
     private final ClienteService clienteService;
