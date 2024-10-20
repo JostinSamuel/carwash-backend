0b21e73d11b5a00cd5c576d8b328a0c5d35f774d
Marcos Basurto <marcosbr301@gmail.com>
2024-10-20 02:46:02 -0500

    No cors UsuarioController.java

--- src/main/java/com/carwash/pe/controller/UsuarioController.java
+++ src/main/java/com/carwash/pe/controller/UsuarioController.java
@@ -3,11 +3,7 @@ package com.carwash.pe.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.carwash.pe.model.Usuario;
 import com.carwash.pe.service.UsuarioService;
@@ -15,14 +11,11 @@ import com.carwash.pe.service.UsuarioService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/usuarios")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200") // Permite solicitudes desde el puerto 4200
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
